6c2fc89d96f27b8a6cdcfeef0c2e0a79090dc21d
dlvadnais <129636431+dlvadnais@users.noreply.github.com>
2023-12-22 19:56:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,34 @@
 from shopping_system import Store, Person
 
 if __name__ == "__main__":
-    # Creating stores with random reputations and cost factors
-    store1 = Store("Mc Ronalds")
-    store2 = Store("Dantes stone emporium")
-    store3 = Store("Clowns R Us")
+    # Dropping in at random stores with vibes
+    store1 = Store("Mc ronalds")
+    store2 = Store("floormart")
+    store3 = Store("arkenanian tire")
 
-    # Creating a person with a random budget
-    shopper = Person("Alice")
+    # Our adventurous shopper with a budget
+    shopper = Person("dave")
 
     # Displaying store information
     store1.get_store_info()
     store2.get_store_info()
     store3.get_store_info()
 
-    # Deciding where to shop
+    # Deciding where to throw some dollars
     shopper.decide_store([store1, store2, store3])
+
+    # Interacting with the chosen store (BudgetBazaar in this case)
+    
+    # Introducing a special offer with a 15% discount and updating the store's cost factor
+    store2.introduce_special_offer(15)
+
+    # Personal shopper activities
+    
+    # Setting a new budget for Alice
+    shopper.set_budget(180)
+
+    # Sharing Alice's shopping experience at BudgetBazaar
+    shopper.share_shopping_experience("BudgetBazaar")
+
+    # Browsing online stores for more shopping options
+    shopper.browse_online_stores()
